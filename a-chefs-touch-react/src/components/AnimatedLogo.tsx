e35630bc30506381268dcbe1df72a960f2f5d7bc
ubadha-kaif <ubadhakaif5766@gmail.com>
2026-03-11 01:31:28 +0530

    style: update logo to minimalist whisk design

--- a-chefs-touch-react/src/components/AnimatedLogo.tsx
+++ a-chefs-touch-react/src/components/AnimatedLogo.tsx
@@ -7,34 +7,33 @@ const AnimatedLogo: React.FC = () => {
             viewBox="0 0 100 100"
             xmlns="http://www.w3.org/2000/svg"
         >
-            {/* Minimalist Chef Hat / Steam outline */}
+            {/* Minimalist Whisk / Continuous Line Art */}
             <path
                 className="logo-path main-path"
-                d="M 30 70 C 20 70 15 60 15 50 C 15 40 25 35 30 35 C 32 20 50 15 60 25 C 65 15 80 20 85 35 C 90 40 90 55 80 65 C 75 70 70 70 30 70 Z"
+                d="M 50 10 
+                   C 50 10, 48 30, 45 45 
+                   C 35 60, 20 70, 35 85 
+                   C 45 95, 55 95, 65 85 
+                   C 80 70, 65 60, 55 45 
+                   C 52 30, 50 10, 50 10 
+                   M 45 45 
+                   C 40 50, 40 70, 50 80 
+                   C 60 70, 60 50, 55 45"
                 fill="none"
                 stroke="currentColor"
-                strokeWidth="4"
+                strokeWidth="3.5"
                 strokeLinecap="round"
                 strokeLinejoin="round"
-            />
-            {/* Inner elegant swoop (like a smile or whisk wire) */}
-            <path
-                className="logo-path accent-path"
-                d="M 35 55 Q 50 65 65 55"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="4"
-                strokeLinecap="round"
             />
             {/* Small elegant dot */}
             <circle
                 className="logo-path dot-path"
                 cx="50"
-                cy="45"
-                r="3"
+                cy="92"
+                r="2.5"
                 fill="none"
                 stroke="currentColor"
-                strokeWidth="4"
+                strokeWidth="2.5"
             />
         </svg>
     );
